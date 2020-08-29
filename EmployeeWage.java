d99486bac4446240a0f9c9565fe267de2b666090
Himanshu <yadavhimanshu63@gmail.com>
2020-08-29 08:37:38 +0530

    Employee Attendance

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,22 @@
  public class EmployeeWage {
-	
-          public static void main(String[] args) {
-          System.out.println("Welcome to Employee Wage Calculation");
-	}
- }
+               //constant
+		private static final int ISPRESENT=1;
+		
+	          //to generate random values	
+		public static void randomCheck() {
+			double empCheck = (int) ((Math.random() * 10) % 2);
+			if (empCheck == ISPRESENT) {
+				System.out.println("Employee is Present");
+			} else {
+				System.out.println("employee is Absent");
+			}
+		}
+
+		public static void main(String[] args) {
+			
+			//printing welcome message
+			System.out.println("Welcome to Employee Wage Computation");
+			EmployeeWage.randomCheck();
+		}
+}	
+
