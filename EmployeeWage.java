fb398cb37f1eb0c603d944293a3a40c5e3812412
Himanshu <yadavhimanshu63@gmail.com>
2020-08-30 21:25:12 +0530

    Part Time

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,31 +1,43 @@
  public class EmployeeWage {
+                //constants
+		private static final int ISFULLTIME=1;
+		private static final int ISPARTTIME=2;
+		private static final int EMPRATE=20;
+		
+	    
+	    
+		public static void empWage() {
+			int empHour = empCheck();
+			int empWage = empHour * EMPRATE;
+			System.out.println("Employee Wage: " + empWage);
+		}
 
-         //constants
-        private static final int ISPRESENT=1;
-	private static final int EMP_RATE_PER_HOUR=20;
-	     
-	    public static void empWage()
-	    {
-	    	int empWage;
-	    	int empHour=0;
-	    	double empCheck = (int) ((Math.random()*10)%2);
-	    	if(empCheck==ISPRESENT)
-	    	{
-	    		System.out.println("Employee is Present");
-	    		empHour=8;
-	    	}
-	    	else
-	    	{
-	    		System.out.println("Employee is Absent");
-	    	}
-	    	empWage=empHour*EMP_RATE_PER_HOUR;
-	    	System.out.println("Employee Wage: " + empWage);
-	    	
-	    }
-	    public static void main(String[] args) {
-	        System.out.println("Welcome to Employee Wage Computation");
-	    	EmployeeWage.empWage();
-			
-		} 
-}	
+		public static int empCheck() {
+			int empHour = 0;
+			int empCheck = (int) ((Math.random() * 10) % 3);
+			switch (empCheck) {
+			case ISFULLTIME:
+				System.out.println("Employee is Present for Full Time");
+				empHour = 8;
+				break;
+
+			case ISPARTTIME:
+				System.out.println("Employee is Present for Part Time");
+				empHour = 4;
+				break;
+
+			default:
+				System.out.println("Employee is Absent");
+				empHour = 0;
+			}
+			return empHour;
+
+		}
+
+		public static void main(String[] args) {
+			System.out.println("Welcome to Employee Wage Computation");
+			EmployeeWage.empWage();
+        }
+}
+	
 
