d5b4a7f381f75ce3b728ce9281ffa7aaa984e6d4
Himanshu <yadavhimanshu63@gmail.com>
2020-08-30 21:36:36 +0530

    Part Time

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,43 +1,39 @@
  public class EmployeeWage {
-                //constants
-		private static final int ISFULLTIME=1;
-		private static final int ISPARTTIME=2;
-		private static final int EMPRATE=20;
-		
+
+            private static final int ISFULLTIME=1;
+	    private static final int ISPARTTIME=2;
+	    private static final int EMP_RATE_PER_HOUR=20;
 	    
 	    
-		public static void empWage() {
-			int empHour = empCheck();
-			int empWage = empHour * EMPRATE;
-			System.out.println("Employee Wage: " + empWage);
+	    
+	    public static void empWage()
+	    {
+	    	int empWage;
+	    	int empHour=0;
+	    	double empCheck = (int) ((Math.random()*10)%3);
+	    	if(empCheck==ISFULLTIME)
+	    	{
+	    		System.out.println("Employee is Present for Full time");
+	    		empHour=8;
+	    	}
+	    	else if(empCheck==ISPARTTIME)
+	    	{
+	    		System.out.println("Employee is Present for Part Time");
+	    		empHour=4;
+	    	}
+	    	else
+	    	{
+	    		System.out.println("Employee is Absent");
+	    	}
+	    	empWage=empHour*EMP_RATE_PER_HOUR;
+	    	System.out.println("Employee Wage: " + empWage);
+	    	
+	    }
+	    public static void main(String[] args) {
+	    	System.out.println("Welcome to Employee Wage Computation");
+	    	EmployeeWage.empWage();
+			
 		}
-
-		public static int empCheck() {
-			int empHour = 0;
-			int empCheck = (int) ((Math.random() * 10) % 3);
-			switch (empCheck) {
-			case ISFULLTIME:
-				System.out.println("Employee is Present for Full Time");
-				empHour = 8;
-				break;
-
-			case ISPARTTIME:
-				System.out.println("Employee is Present for Part Time");
-				empHour = 4;
-				break;
-
-			default:
-				System.out.println("Employee is Absent");
-				empHour = 0;
-			}
-			return empHour;
-
-		}
-
-		public static void main(String[] args) {
-			System.out.println("Welcome to Employee Wage Computation");
-			EmployeeWage.empWage();
-        }
 }
 	
 
